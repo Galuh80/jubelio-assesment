3073b37bc3a1d5421db35faea57dba70882d75d9
Galuh80 <galuh.ibrahiim@gmail.com>
2024-08-04 21:16:25 +0700

    add: swagger UI

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
 require('dotenv').config();
 const productRoutes = require('./routes/ProductRoutes');
 const transactionRoutes = require('./routes/TransactionRoutes');
@@ -9,11 +12,29 @@ const init = async () => {
         port: process.env.APP_PORT
     });
 
+    const swaggerOptions = {
+        info: {
+            title: 'Jubelio Assessment Simple Transaction API with Swagger',
+            version: '0.1.0',
+            description: 'This is a simple CRUD API application made with NodeJS, HapiJS and documented with Swagger',
+        },
+    };
+
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
     server.route(productRoutes);
     server.route(transactionRoutes);
 
     await server.start();
     console.log(`Server running on port ${process.env.APP_PORT}`);
+    console.log(`Swagger documentation available at ${server.info.uri}/documentation`);
 };
 
 process.on('unhandledRejection', (err) => {
